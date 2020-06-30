6347f8cf1fd48d9d145ae76f1da53020d97b8d73
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2020-06-30 22:07:07 +0200

    added renderTasks fuction

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
 {
-    
+    const taskTable = [
+        {
+            content: "Test task",
+            done: true,
+        },
+    ];
+
+    const renderTasks = () =>{
+        htmlTasksList = "";
+
+        for(const task of taskTable){
+            htmlTasksList += `
+            <li>
+                ${task.content}
+            </li>
+            `;
+
+        }
+
+        document.querySelector('.js-tasks').innerHTML = htmlTasksList;
+    };
+
+    const init = () =>{
+        renderTasks();
+    };
+
+    init();
 }
